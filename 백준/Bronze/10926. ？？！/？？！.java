d95ff6ac2b69d1911443381988af2ff982e2d496
jaeheoon <132314644+jaeheoon@users.noreply.github.com>
2023-06-03 17:23:01 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		String a = "??!";
+		System.out.println(name + a);
+	}
+
+}
