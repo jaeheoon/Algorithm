bf7569287ea582ce30725bb67301b907b0a79617
jaeheoon <132314644+jaeheoon@users.noreply.github.com>
2023-06-07 10:38:06 +0900

    [Bronze V] Title: A+B - 8, Time: 272 ms, Memory: 18960 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		int sum = 0;
+		
+		for (int i = 1; i <= t; i++) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			sum = a+b;
+			System.out.println("Case #"+i+": "+a+" + "+b+" = "+sum);
+		}
+		
+	}
+}
