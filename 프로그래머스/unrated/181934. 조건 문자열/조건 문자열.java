27f16ba07485a36a8351945629a5482f2f78b749
jaeheoon <132314644+jaeheoon@users.noreply.github.com>
2023-06-18 15:41:05 +0900

    [unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
@@ -1,6 +1,17 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
         int answer = 0;
+       if(60 == ineq.charAt(0)){
+            if(63 != eq.charAt(0))
+            	answer = n <= m ? 1 : 0;
+            else
+            	answer = n < m ? 0 : 1;
+        } else {
+            if(63 != eq.charAt(0))
+            	answer = n >= m ? 1 : 0;
+            else
+            	answer = n > m ? 0 : 1;
+        }
         return answer;
     }
 }
