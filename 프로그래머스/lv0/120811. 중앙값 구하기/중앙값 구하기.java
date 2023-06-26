aba8df8e4d1d4af4cac1abc194b7ca53639c61aa
jaeheoon <132314644+jaeheoon@users.noreply.github.com>
2023-06-26 15:44:21 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.33 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -2,11 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] array) {
-        int answer = 0;
         Arrays.sort(array);
-    	 for (int i = 0; i < array.length; i++) {
-			answer = array[array.length/2];
-		}
-         return answer;
+    	 return array[array.length/2];
     }
 }
