6eebe82358e70e122bc7a52224c7eb8edaa9e566
jaeheoon <132314644+jaeheoon@users.noreply.github.com>
2023-06-27 10:22:49 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,5 +1,5 @@
 class Solution {
     public int solution(int slice, int n) {
-        return (n+slice) % slice == 0 ? ((n+slice) / slice) - 1 : (n+slice) / slice;
+        return n % slice > 0 ? n / slice + 1 : n / slice;
     }
 }
