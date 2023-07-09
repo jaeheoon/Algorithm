05ed137034b63154ed75510b99b2fcaedba3a4ac
jaeheoon <132314644+jaeheoon@users.noreply.github.com>
2023-07-09 14:37:53 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(int n, int t) {
-        int answer = 0;
-		for (int i = 0; i < t; i++) {
-			answer += n + answer;
-		}
-		return answer + n;
+        for(int i = 0; i < t; i++) {
+            n *= 2;
+        }
+        return n;
     }
 }
