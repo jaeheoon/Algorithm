50bf8bbd31f61658b4d7e917f4f9b00f3bf0ff4d
jaeheoon <132314644+jaeheoon@users.noreply.github.com>
2023-07-09 15:14:06 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String a = String.valueOf(order);
+        for (int i = 0; i < a.length(); i++) {
+			if (a.charAt(i) == '3' || a.charAt(i) == '6' || a.charAt(i) == '9') {
+				answer++;
+			}
+		}
+        return answer;
+    }
+}
