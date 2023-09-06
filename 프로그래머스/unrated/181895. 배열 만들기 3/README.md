e46de3fa0d662b35dbcad61b5f9464a9af31f10f
jaeheoon <132314644+jaeheoon@users.noreply.github.com>
2023-09-06 17:31:01 +0900

    [unrated] Title: 배열 만들기 3, Time: 1.19 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/README.md
+++ 프로그래머스/unrated/181895. 배열 만들기 3/README.md
@@ -1,10 +1,10 @@
 # [unrated] 배열 만들기 3 - 181895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181895?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181895?language=java#) 
 
 ### 성능 요약
 
-메모리: 67.6 MB, 시간: 1.05 ms
+메모리: 77 MB, 시간: 1.19 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -3,11 +3,10 @@ import java.util.ArrayList;
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
         ArrayList<Integer> a = new ArrayList<Integer>();
-		for (int i = intervals[0][0]; i < intervals[0][1] + 1; i++) {
-			a.add(arr[i]);
-		}
-		for (int j = intervals[1][0]; j < intervals[1][1] + 1; j++) {
-			a.add(arr[j]);
+		for (int i = 0; i < intervals.length; i++) {
+			for (int j = intervals[i][0]; j <= intervals[i][1]; j++) {
+				a.add(arr[j]);
+			}
 		}
 		int[] answer = new int[a.size()];
 		for (int i = 0; i < a.size(); i++) {
