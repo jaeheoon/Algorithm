1becbdb341493a3f3bbce4f875e1abf40d26e9df
jaeheoon <aleh101@naver.com>
2023-10-27 19:45:11 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        while(n != 0) {
+            list.add(n % 3);
+            n /= 3;
+        } 
+        
+        int a = 1;
+        for(int i=list.size()-1; i>=0; i--) {
+            answer += list.get(i) * a;
+            a *= 3;
+        }
+        return answer;
+    }
+}
