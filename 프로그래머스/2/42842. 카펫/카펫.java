982cc808a3774b3e92de3a7b33acc0aa03334d8e
jaeheoon <aleh101@naver.com>
2023-11-20 20:51:53 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,23 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int sum = brown + yellow;
-        
-        for (int i = 3; i < sum; i++) {
-            int j = sum / i;
-            
-            if (sum % i == 0 && j >= 3) {
-                int col = Math.max(i, j);
-                int row = Math.min(i, j);
-                int center = (col - 2) * (row - 2);
-                
-                if (center == yellow) {
-                    answer[0] = col;
-                    answer[1] = row;
-                    return answer;
+        for (int width = 3; width <= 5000; width++) {
+            for (int height = 3; height <= width; height++) {
+                int boundary = (width + height - 2) * 2;
+                int center = width * height - boundary;
+
+                if (brown == boundary && yellow == center) {
+                    return new int[] {width, height};
                 }
             }
         }
-        return answer;
+        return null;
     }
 }
