778ab4a67beeda26c5f55a1a863a290fafb1e3ad
jaeheoon <aleh101@naver.com>
2024-01-03 16:31:04 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    private static final int MEMBER_PERIOD = 10;
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        Map<String,Integer> map = new HashMap<>();
+        int index = 0;
+        for(String str : want){
+            map.put(str,index++);
+        }
+        for(int i = 0 ; i < discount.length ; i++){
+            if(i >= MEMBER_PERIOD){
+                index = map.getOrDefault(discount[i - MEMBER_PERIOD],-1);
+                if(index != -1){
+                    number[index]++;
+                }
+            }
+            String str  = discount[i];
+            index = map.getOrDefault(str,-1);
+            if(index != -1){
+                number[index]--;
+            }
+            if(checkNumber(number)){
+                answer++;
+            }
+        }
+        return answer;
+    }
+    private boolean checkNumber(int[] number){
+        for(int num : number){
+            if(num > 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
