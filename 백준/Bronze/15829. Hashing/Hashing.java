549cd12addeefd83219d1839c7dc412033bb8c3d
jaeheoon <aleh101@naver.com>
2024-03-02 00:42:48 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 14140 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int prime = 1234567890;
+        long r = 1;
+        long sum = 0;
+        
+        for(int i = 0; i < N; i++) {
+            sum += ((s.charAt(i) - 'a' + 1) * r) % prime;
+            r = (r * 31) % prime;
+        }
+        
+        System.out.println(sum % prime);
+    }
+}
