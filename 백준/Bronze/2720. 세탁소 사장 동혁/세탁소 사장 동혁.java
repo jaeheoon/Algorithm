9bf07b223c85d85db24c5de0a1aa64c7b08fc525
jaeheoon <aleh101@naver.com>
2024-03-09 08:48:09 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 148 ms, Memory: 14772 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        
+        int quarter = 25;
+        int dime = 10;
+        int nickel = 5;
+        int penny = 1;
+        
+        for (int i = 0; i < t; i++) {
+            int c = Integer.parseInt(br.readLine());
+            sb.append(c / quarter + " ");
+            c %= quarter;
+            sb.append(c / dime + " ");
+            c %= dime;
+            sb.append(c / nickel + " ");
+            c %= nickel;
+            sb.append(c / penny + "\n");
+            
+        }
+        
+        System.out.print(sb);
+    }
+}
