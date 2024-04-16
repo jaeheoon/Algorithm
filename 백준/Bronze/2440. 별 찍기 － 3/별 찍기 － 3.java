cb445c0a3b3e9d82abde9285f74f53e9410ab7c4
jaeheoon <aleh101@naver.com>
2024-04-16 21:31:47 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
