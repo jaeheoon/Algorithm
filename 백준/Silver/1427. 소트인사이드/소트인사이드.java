2adf1d59161956cafc36baf16c4ec893ad7ccd0d
jaeheoon <aleh101@naver.com>
2024-05-08 20:30:44 +0900

    [Silver V] Title: 소트인사이드, Time: 136 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String N = br.readLine();
+        br.close();
+
+        Integer[] array = new Integer[N.length()];
+
+        for(int i = 0; i < array.length; i++) {
+            array[i] = N.charAt(i) - '0';
+        }
+
+        Arrays.sort(array, Collections.reverseOrder());
+
+        for(int i = 0; i < array.length; i++) {
+            bw.write(array[i]+"");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
