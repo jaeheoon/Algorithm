4816bea0038696e3240cbfb6044aefad168447ee
jaeheoon <aleh101@naver.com>
2024-11-27 11:45:20 +0900

    [level 1] Title: 예산, Time: 0.53 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,14 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int total = 0;
 	    Arrays.sort(d);
 	    for(int i=0; i<d.length; i++){
-	      if(total + d[i] <= budget) {
-            total += d[i];
-	    	answer++;
-	        }
-         }
+            if(d[i] <= budget) {
+                answer++;
+                budget -= d[i];
+            }
+        }
 	    return answer;
     }
 }
